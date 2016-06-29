137d5c2461bd213ef8e1a404da15a415998b6887
Nick Smith ♞ <npsmith1990@gmail.com>
2016-06-29 06:14:55 -0500

    Create snippet for functional stateless component

---
+++ stateless_functional_component.js
@@ -0,0 +1,9 @@
+<snippet>
+    <content><![CDATA[
+import React from 'react';
+const ${1:${TM_FILENAME/(.?\w*)(?:\.\w+)*$/$1/g}} = () => <${5:div}></${5:div}>
+]]></content>
+    <tabTrigger>sfc</tabTrigger>
+    <scope>source.js</scope>
+    <description>React: stateless functional component</description>
+</snippet>
